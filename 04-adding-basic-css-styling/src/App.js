570cf5153769bf8fe660e3407f6233747afad60d
Sourabh Sanjay Hawale <69086016+Sourabh-1008@users.noreply.github.com>
2022-12-22 11:01:39 +0530

    commit

---
+++ 04-adding-basic-css-styling/src/App.js
@@ -0,0 +1,11 @@
+import ExpenseItem from "./componsnts/ExpenseItem";
+function App() {
+  return (
+    <div>
+      <h2>Let's get started!</h2>
+      <ExpenseItem />
+    </div>
+  );
+}
+
+export default App;
